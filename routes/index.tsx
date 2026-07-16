dd891458fe3b3a300c3450e19bf3588e5f77ae94
Abhinandh S <abhinandhsuby@proton.me>
2026-07-16 19:40:14 +0530

    Update index.tsx

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,6 +1,7 @@
 import { useSignal } from "@preact/signals";
 import { define } from "../utils.ts";
 import Counter from "../islands/Counter.tsx";
+import getYoutubeChannelId from "../islands/inputBar.tsx";
 
 export default define.page<typeof handlers>(function Home(ctx) {
   const count = useSignal(3);
@@ -37,13 +38,23 @@ export const handlers = define.handlers({
   },
   async POST(ctx) {
     const form = await ctx.req.formData();
-    const email = form.get("username")?.toString();
+    const input = form.get("username")?.toString();
 
     // Add email to list.
+const username = input.trim();
+
+  const channelId = await getYoutubeChannelId(username);
+
+  if (!channelId) {
+  //  reply("Sorry, I couldn't find a channel with that username/handle.")
+  }
+ // const rssUrl = `https://www.youtube.com/feeds/videos.xml?channel_id=${channelId}`
+
+
 
     // Redirect user to thank you page.
     const headers = new Headers();
-    headers.set("location", "/thanks-for-subscribing");
+    headers.set("location", "/${channelId}");
     return new Response(null, {
       status: 303, // See Other
       headers,
