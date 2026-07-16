5dfcd138e33ac4fdebec7bede0c78c6a9714dae1
Abhinandh S <abhinandhsuby@proton.me>
2026-07-16 18:57:13 +0530

    Update index.tsx

--- routes/index.tsx
+++ routes/index.tsx
@@ -2,13 +2,14 @@ import { useSignal } from "@preact/signals";
 import { define } from "../utils.ts";
 import Counter from "../islands/Counter.tsx";
 
-export default define.page(function Home(ctx) {
+export default define.page<typeof handlers>(function Home(ctx) {
   const count = useSignal(3);
 
   ctx.state.title = count.value + " Fresh Counter" +
     (Math.abs(count.value) === 1 ? "" : "s");
 
   return (
+<>
     <div class="px-4 py-8 mx-auto fresh-gradient min-h-screen">
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
         <img
@@ -26,5 +27,30 @@ export default define.page(function Home(ctx) {
         <Counter count={count} />
       </div>
     </div>
+    <form method="post">
+        <input type="email" name="email" value="" />
+        <button type="submit">Subscribe</button>
+      </form>
+  </>
   );
+});
+
+export const handlers = define.handlers({
+  async GET(ctx) {
+    return { data: {} };
+  },
+  async POST(ctx) {
+    const form = await ctx.req.formData();
+    const email = form.get("email")?.toString();
+
+    // Add email to list.
+
+    // Redirect user to thank you page.
+    const headers = new Headers();
+    headers.set("location", "/thanks-for-subscribing");
+    return new Response(null, {
+      status: 303, // See Other
+      headers,
+    });
+  },
 });
