800ba30b453e060421b90d4eaafac7cea5a46a21
Abhinandh S <abhinandhsuby@proton.me>
2026-07-16 20:05:19 +0530

    Update [username].tsx

--- routes/[username].tsx
+++ routes/[username].tsx
@@ -3,8 +3,20 @@ import { define } from "../utils.ts";
 export const handler = define.handlers({
   GET(ctx) {
     const username = ctx.params.username;
-    return new Response(
-      `Hello, ${username.charAt(0).toUpperCase() + username.slice(1)}!`,
+    return (
+       <div class="px-4 py-8 mx-auto fresh-gradient min-h-screen">
+      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
+        <img
+          class="my-6"
+          src="/logo.svg"
+          width="128"
+          height="128"
+          alt="the Fresh logo: a sliced lemon dripping with juice"
+        />
+        <h1 class="text-4xl font-bold">Just provide @username and get RSS link for that channel.</h1>
+        <input type="text" value=`${username}` id="myInput">
+      </div>
+    </div>
     );
   },
 });
