1a6c9baf7fbafd2f62b9b8d6a63d060d4a897615
Abhinandh S <abhinandhsuby@proton.me>
2026-07-16 21:23:47 +0530

    Update [id].tsx

--- routes/[id].tsx
+++ routes/[id].tsx
@@ -12,12 +12,11 @@ export default define.page<typeof handler>(function Page({ data }) {
   const { channelId } = data;
 
   return (
-    <div class="px-4 py-8 mx-auto fresh-gradient min-h-screen">
+    <div class="p-8 min-h-screen">
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <h1 class="text-4xl font-bold mb-60">Your RSS Link is Ready!</h1>
+        <h1 class="text-2xl font-bold mb-8">Your RSS Link is Ready!</h1>
         <p class="text-gray-600 mb-6 pb-10">Click below to copy your YouTube RSS Feed URL.</p>
 
-        {/* Pass the server-side parameter into your interactive island */}
         <CopyLink channelId={channelId} />
 
         <a href="/" class="mt-16 text-sm text-blue-500 hover:underline">
