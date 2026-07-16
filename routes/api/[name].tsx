af13801bdc93c2fa864c384c9f5637ad22ce2e6c
Abhinandh S <abhinandhsuby@proton.me>
2026-07-16 21:16:52 +0530

    Update [name].tsx

--- routes/api/[name].tsx
+++ routes/api/[name].tsx
@@ -2,9 +2,10 @@ import { define } from "../../utils.ts";
 
 export const handler = define.handlers({
   GET(ctx) {
-    const name = ctx.params.name;
+    const name = ;
+      const rssUrl = `https://www.youtube.com/feeds/videos.xml?channel_id=${ctx.params.name}`;
     return new Response(
-      `Hello, ${name.charAt(0).toUpperCase() + name.slice(1)}!`,
+      `${rssUrl}`,
     );
   },
 });
