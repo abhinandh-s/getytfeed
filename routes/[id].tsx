b62383f2b628bc79d1899f6bd167b615f67deb1b
Abhinandh S <abhinandhsuby@proton.me>
2026-07-16 21:27:02 +0530

    Update [id].tsx

--- routes/[id].tsx
+++ routes/[id].tsx
@@ -14,8 +14,8 @@ export default define.page<typeof handler>(function Page({ data }) {
   return (
     <div class="p-8 min-h-screen">
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <h1 class="text-2xl font-bold">Your RSS Link is Ready!</h1>
-        <p class="text-gray-600">Click below to copy your YouTube RSS Feed URL.</p>
+        <h1 class="text-2xl font-bold my-12">Your RSS Link is Ready!</h1>
+        <p class="text-gray-600 mb-8">Click below to copy your YouTube RSS Feed URL.</p>
 
         <CopyLink channelId={channelId} />
 
